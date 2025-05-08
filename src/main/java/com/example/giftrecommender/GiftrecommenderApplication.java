91dcb277443d6d147d1ab3a3290bea22d45615e5
HyeonJin Song <shwj1602@naver.com>
2025-05-08 18:22:14 +0900

    init: 프로젝트 초기 설정 및 기본 구조 세팅

---
+++ src/main/java/com/example/giftrecommender/GiftrecommenderApplication.java
@@ -0,0 +1,13 @@
+package com.example.giftrecommender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftrecommenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiftrecommenderApplication.class, args);
+	}
+
+}
