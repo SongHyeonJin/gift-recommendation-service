825d19f61c2444301394cdf3372d02f8a894b6b4
HyeonJin Song <shwj1602@naver.com>
2025-05-17 12:51:50 +0900

    docs(recommendation): Swagger 예시 키워드 목록 수정

--- src/main/java/com/example/giftrecommender/dto/request/RecommendationRequestDto.java
+++ src/main/java/com/example/giftrecommender/dto/request/RecommendationRequestDto.java
@@ -5,6 +5,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
 
 public record RecommendationRequestDto(
-        @Schema(description = "대표 키워드 목록", example = "[\"연인\", \"5만원 이상 10만원 이하\", \"운동\", \"다이어트\"]")
+        @Schema(description = "대표 키워드 목록", example = "[\"여자친구\", \"5~10만원\", \"생일\", \"악세서리\", \"우아한\"]")
         List<String> keywords
 ) {}
