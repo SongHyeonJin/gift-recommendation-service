b7c27b27c87cfee20f6239c8c48eefd9c029b65a
HyeonJin Song <shwj1602@naver.com>
2025-05-22 17:36:08 +0900

    test(user-answer): 고정형 유저 응답 저장 컨트롤러 WebMvc 테스트 추가

---
+++ src/test/java/com/example/giftrecommender/controller/UserAnswerControllerTest.java
@@ -0,0 +1,61 @@
+package com.example.giftrecommender.controller;
+
+import com.example.giftrecommender.domain.enums.QuestionType;
+import com.example.giftrecommender.dto.request.UserAnswerRequestDto;
+import com.example.giftrecommender.service.UserAnswerService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.UUID;
+
+import static org.mockito.Mockito.doNothing;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@ActiveProfiles("test")
+@WebMvcTest(UserAnswerController.class)
+class UserAnswerControllerTest {
+
+    @Autowired private MockMvc mockMvc;
+
+    @Autowired private ObjectMapper objectMapper;
+
+    @MockBean private UserAnswerService userAnswerService;
+
+    private UUID guestId;
+
+    private UUID sessionId;
+
+    @BeforeEach
+    void setUp() {
+        guestId = UUID.randomUUID();
+        sessionId = UUID.randomUUID();
+    }
+
+    @DisplayName("POST /answers - 유저 응답 저장 성공")
+    @Test
+    void saveUserAnswerSuccess() throws Exception {
+        // given
+        UserAnswerRequestDto requestDto = new UserAnswerRequestDto(1L, QuestionType.CHOICE,1L);
+        doNothing().when(userAnswerService).saveAnswer(guestId, sessionId, requestDto);
+
+        // when & then
+        mockMvc.perform(post("/api/guests/{guestId}/recommendation-sessions/{sessionId}/answers", guestId, sessionId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(requestDto)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.message").value("응답 저장"))
+                .andExpect(jsonPath("$.data").doesNotExist());
+    }
+
+}
