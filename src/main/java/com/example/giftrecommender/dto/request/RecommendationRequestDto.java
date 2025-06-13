42f2058668b0f1543cd8775114d49b0dafba5d8a
HyeonJin Song <shwj1602@naver.com>
2025-06-13 17:42:47 +0900

    feat(dto): 추천 요청 DTO에 관계 및 가격 필드 추가

--- src/main/java/com/example/giftrecommender/dto/request/RecommendationRequestDto.java
+++ src/main/java/com/example/giftrecommender/dto/request/RecommendationRequestDto.java
@@ -5,6 +5,12 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
 
 public record RecommendationRequestDto(
-        @Schema(description = "대표 키워드 목록", example = "[\"여자친구\", \"5~10만원\", \"생일\", \"악세서리\", \"우아한\"]")
+        @Schema(description = "관계", example = "남자친구")
+        String relation,
+        @Schema(description = "최소 가격", example = "50000")
+        int minPrice,
+        @Schema(description = "최대 가격", example = "100000")
+        int maxPrice,
+        @Schema(description = "대표 키워드 목록", example = "[\"러닝화\", \"러닝가방\", \"바람막이\", \"스마트워치\"]")
         List<String> keywords
 ) {}
